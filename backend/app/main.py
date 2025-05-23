a2242b9b783d2f69f34bf1b977570b25f83db120
Muddisetti Hemanth Kumar <083hemanth@gmail.com>
2025-05-23 14:10:36 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI  # type: ignore
 from app.api import routes_upload, routes_qa, routes_docs, routes_users
 from fastapi.middleware.cors import CORSMiddleware  # type: ignore
 from dotenv import load_dotenv  # type: ignore
+import uvicorn
 
 load_dotenv()
 
@@ -26,3 +27,6 @@ app.include_router(routes_upload.router, prefix="/upload", tags=["Upload"])
 app.include_router(routes_qa.router, prefix="/ask", tags=["QA"])
 app.include_router(routes_docs.router, prefix="/docs", tags=["Documents"])
 app.include_router(routes_users.router, prefix = "/users", tags=["Documents"])
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
